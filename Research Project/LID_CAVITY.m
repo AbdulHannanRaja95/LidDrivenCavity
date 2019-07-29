3c246b3b948466c69af73daf0d6ece6633c269a1
Abdul Hannan <52470644+AbdulHannanRaja95@users.noreply.github.com>
2019-07-30 02:30:11 +0500

    commenting done

--- Research Project/LID_CAVITY.m
+++ Research Project/LID_CAVITY.m
@@ -19,17 +19,18 @@ for i=1:nx
     end
 end
 dt = 0.001; %time step
-t = 1; % initial iteration number
+t = 1; % iteration number
 
 %%Boundary Conditions Specification/preallocation
 u(1:nx,1:ny) = 0;  % initially u = 0 over the entire domain
-u(1:nx,ny)   = 1;  % initially u = 1 over the right boundary
+u(1:nx,ny)   = 1;  % initially u = 1 over the right boundary(although the according to the problem statement,it is false..this 
+% does not make any sense..) it should be u(nx,1:ny) = 1 which indicates the lid is being moved with velocity in x-direction..
 v(1:nx,1:ny) = 0;  % initially v = 0 over the entire domain
 
 p(1:nx,1:ny) = 0.5; % initially pressure = 0.5 over the entire domain
 Re = 500; %reynold number for this problem
 b = 1/0.6; % artificial compressibility factor
-er = 100; % initial error 
+er = 100; % error initialization
 
 tic % timer starts for the computation 
 
@@ -38,42 +39,44 @@ tic % timer starts for the computation
 
 
 
-while(((er >= 1e-5) && (t <= 10000)))
+while(((er >= 1e-5) && (t <= 10000)))%error condition is always true....time condition gets violated which results in stopage of code 
     
-    %u1,v1 and p1  are computed at different time from actual u,v and p.
-    
+    %u1,v1 and p1  are computed at different time from actual u,v and p. u1,v1 and p1 are actually storage variable.they store the data 
+    %computed at each and every iteration
     u1 = u;
     v1 = v;
     p1 = p; 
-    % pressure calculation from continuity eqaution with artificial
-    % compressibility factor incorporated
-    for i=2:nx-1
-        for j=2:ny-1
-            p(i,j)=p1(i,j) - dt*b*(0.5/dx*(u(i+1,j)-u(i-1,j))+0.5/dy*(v(i,j+1)-v(i,j-1))); 
+    % pressure calculation from continuity eqaution with artificial compressibility factor incorporated
+    for i=2:nx-1%to incorporate the center difference scheme for fist order derivative of u in x direction
+        for j=2:ny-1%to incorporate the center difference scheme for fist order derivative of u in y direction
+            p(i,j)=p1(i,j) - dt*b*(0.5/dx*(u(i+1,j)-u(i-1,j))+0.5/dy*(v(i,j+1)-v(i,j-1))); %FT for prssure term, CS for convective terms
+            %p is the pressure at next time step(n+1) while p1 is at previous time step(n) 
         end
     end
-    
+    %not sure about this part...it actually equals pressure for all 4 boundary nodes to the penultimate nodes...i think just to make dp/dy=0  
     p(1,:) = p(2,:);
     p(nx,:) = p(nx-1,:);
     p(:,1) = p(:,2);
     p(:,ny) = p(:,ny-1);
-    % 
+    % This part solves x-momentum equation in non-dimensional form..CS scheme is used for Second order derivative terms and also for the
+    % first order term.
     for i=2:nx-1
          for j=2:ny-1
             
             RHSu = 1/Re*(1/dx^2*(u1(i+1,j)-2*u1(i,j)+u1(i-1,j))+1/dy^2*(u1(i,j+1)-2*u1(i,j)+u1(i,j-1)))-(p(i+1,j)-p(i-1,j))/(2*dx);
             LHSu = 0.5/dx*(u1(i+1,j)^2-u1(i-1,j)^2)+0.5/dy*(u1(i,j+1)*v1(i,j+1)-u1(i,j-1)*v1(i,j-1));
-            u(i,j) = u1(i,j) + dt*RHSu - dt*LHSu;
+            u(i,j) = u1(i,j) + dt*RHSu - dt*LHSu; %FT for time derivative term, u is calculated at u(n+1) while u1 is calculated at u1(n)
          end
     end
-  
-
+    
+    % This part solves y-momentum equation in non-dimensional form..CS scheme is used for Second order derivative terms and also for the
+    % first order term.
     for i=2:nx-1
         for j=2:ny-1
             
             RHSv = 1/Re*(1/dx^2*(v1(i+1,j)-2*v1(i,j)+v1(i-1,j))+1/dy^2*(v1(i,j+1)-2*v1(i,j)+v1(i,j-1)))-(p(i,j+1)-p(i,j-1))/(2*dy);
             LHSv = 0.5/dy*(v1(i,j+1)^2-v1(i,j-1)^2)+0.5/dx*(u1(i+1,j)*v1(i+1,j)-u1(i-1,j)*v1(i-1,j));
-            v(i,j) = v1(i,j) + dt*RHSv - dt*LHSv;
+            v(i,j) = v1(i,j) + dt*RHSv - dt*LHSv;%FT for time derivative term, again v(n+1) and v1(n)
         end
     end
     
@@ -97,7 +100,7 @@ toc % timer stops for the computation
 
 for i=1:nx-1 %to cater for forward difference scheme
     for j=1:ny-1 %to cater for forward difference scheme
-        vor(i,j) = 1/dy*(u(i,j+1)-u(i,j)) - 1/dx*(v(i+1,j)-v(i,j));%loop starts from 1 that's why center diff is not used, forward difference is used
+        vor(i,j) = 1/dy*(u(i,j+1)-u(i,j)) - 1/dx*(v(i+1,j)-v(i,j));%loop starts from 1 , center diff is not used, forward difference is used
     end
 end
 vor(nx,:) = vor(nx-1,:);
